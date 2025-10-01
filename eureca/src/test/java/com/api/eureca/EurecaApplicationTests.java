32bcb9007694e28b235a7a9969df9b15d00e76fe
K M Farhat Snigdah <farhatsnigdho1392@gmail.com>
2025-10-01 12:52:21 +0600

    initilize project"

---
+++ eureca/src/test/java/com/api/eureca/EurecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.eureca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
